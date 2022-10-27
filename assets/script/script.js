fbb657677142026934d95fd17e85efab2d452d38
Blaz <blaz.kosica@gmail.com>
2022-10-27 10:26:24 +0000

    Add elements to Open hours and style, back on top function

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,1 +1,10 @@
-console.log("It works")
+// Back on top function
+const backOnTop = document.querySelector(".back-on-top");
+
+window.addEventListener("scroll", () => {
+    if (window.pageYOffset > 200) {
+        backOnTop.classList.add("active");
+    } else {
+        backOnTop.classList.remove("active");
+    }
+})
